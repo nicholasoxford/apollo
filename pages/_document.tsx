249f8058c1ded657736e5844b6a9c41b293e90ba
nicholas o <nicholas@presssportsapp.com>
2023-03-06 15:47:58 -0700

    og image fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     let description = "Mintee is the fastest NFT minting service.";
-    let ogimage = "https://www.roomgpt.io/og-image.png";
+    let ogimage = "https://minte.vercel.app/public/warp.jpeg";
     let sitename = "Mintee";
     let title = "Fastest NFT minting service";
 
